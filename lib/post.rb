39222a09890c74cb15a4c517e98fd2ebd6559e0f
Brennan Blair <jbrennan.blair@gmail.com>
2018-09-20 08:53:04 -0400

    Done

---
+++ lib/post.rb
@@ -0,0 +1,29 @@
+require 'pry'
+
+class Post
+  attr_accessor :title, :author
+
+  @@all = []
+
+  def initialize(title)
+    @title = title
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def title
+    @title
+  end
+
+  def author_name
+    if author != nil
+      self.author.name
+    end
+  end
+
+
+
+end #End of Post Class
