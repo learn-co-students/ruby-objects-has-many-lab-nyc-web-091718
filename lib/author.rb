6a50c2a9fa62fc9ec98c00dcaf73a4c630bc4d21
Kristopher <kris.bullinger@gmx.com>
2018-09-19 21:41:55 -0400

    Lab Complete

---
+++ lib/author.rb
@@ -0,0 +1,27 @@
+class Author
+  attr_accessor :name
+  @@posts = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def posts
+    @@posts
+  end
+
+  def add_post(post)
+    @@posts << post
+    post.author = self
+  end
+
+  def add_post_by_title(title)
+    new_post = Post.new(title)
+    new_post.author = self
+    @@posts << new_post
+  end
+
+  def self.post_count
+    Post.all.count
+  end
+end
